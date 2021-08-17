a50a3bfb20765030851bc439c3ad17707b4ee3be
KristineAlbers <1259@rts-365.dk>
2021-08-17 14:04:53 +0200

    incl js

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -2,6 +2,30 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+    let textCount = document.querySelector('#text-count');
+    let number = 5;
+    updateNumber();
+    
+    //Funktion plusknap
+    var btn = document.getElementById('btn-count-increase');
+    btn.addEventListener('click', () => {
+        if(number < 10){
+        number++;
+        updateNumber();
+}});
+
+    //Funktion minusknap
+    var btnM = document.getElementById('btn-count-decrease');
+    btnM.addEventListener('click', () => {
+        if(number > 0){
+        number--;
+        updateNumber();
+}});
+
+    //function der kan erstatte linjerne med: textCount.textContent = number;
+    function updateNumber(){
+        textCount.textContent = number;
+    }
+
 
 }); // Afslutter: DOMContentLoaded
