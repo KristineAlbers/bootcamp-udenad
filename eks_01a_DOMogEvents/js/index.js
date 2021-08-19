dad1ef2b8d554e32193b75c5d92949a20409e2f6
KristineAlbers <1259@rts-365.dk>
2021-08-19 10:04:45 +0200

    add countMinMax

--- eks_01a_DOMogEvents/js/index.js
+++ eks_01a_DOMogEvents/js/index.js
@@ -2,32 +2,36 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-    let textCount = document.querySelector('#text-count');
+    
     let minValue = 0;
     let number = 5;
     let maxValue = 10;
-    updateNumber();
-    
-    //Funktion plusknap
-    var btn = document.getElementById('btn-count-increase');
-    btn.addEventListener('click', () => {
-        if(number < maxValue){
-        number++;
-        updateNumber();
-}});
-
-    //Funktion minusknap
-    var btnM = document.getElementById('btn-count-decrease');
-    btnM.addEventListener('click', () => {
-        if(number > minValue){
-        number--;
-        updateNumber();
-}});
-
-    //function der kan erstatte linjerne med: textCount.textContent = number;
-    function updateNumber(){
-        textCount.textContent = number;
+    let plusBotton = document.querySelector('#btn-count-increase');
+    let minusBotton = document.querySelector('#btn-count-decrease');
+    let textCountElement = document.querySelector('#text-count');
+ 
+    function updateNumber() {
+        textCountElement.textContent = number;
     }
+    updateNumber();
+
+    function checkMinMaxValue(direction) {
+        if (direction === 'inc') {
+            if (number < maxValue) {
+                number++;
+                updateNumber();
+            }
+        }
+        else if (direction === 'dec') {
+            if (number > minValue) {
+                number--;
+                updateNumber();
+            }
+        }
+    }
+    
+    plusBotton.addEventListener('click', () => checkMinMaxValue('inc'));
+    minusBotton.addEventListener('click', () => checkMinMaxValue('dec'));
 
 
 }); // Afslutter: DOMContentLoaded
