6279153bafeeb22b903a0ca55bbd667bdb90d4ac
KristineAlbers <1259@rts-365.dk>
2021-08-19 12:42:19 +0200

    add js

--- eks_02b_funktioner/js/index.js
+++ eks_02b_funktioner/js/index.js
@@ -1,6 +1,29 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
+	// function navn(parameter) {
+	function hils(navn) {
+		console.log(`Hej med dig ${navn}`);
+		console.log(`Hej med dig
+		
+	${navn}`);
+	}
 
-	// skriv din kode her...
+	// Kalder:
+	// navn('argument'):
+	hils('Kristine ');
+	hils('Maggi ');
+	hils('Joey ');
+
+
+	/*
+	// function navn(parameter) {
+		function hils(navn, efternavn) {
+			console.log('Hej med dig, ' + navn + efternavn);
+		};
+		// Kalder:
+		// navn('argument'):
+		hils('Kristine ', 'Albers');
+	*/	
+	
 
 }); // Afslutter: DOMContentLoaded
