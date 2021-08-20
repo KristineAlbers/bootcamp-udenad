db61881f85861cce415658c4264bd94434f53421
KristineAlbers <1259@rts-365.dk>
2021-08-20 09:24:49 +0200

    add more arrays

--- eks_03a_arrays/js/index.js
+++ eks_03a_arrays/js/index.js
@@ -1,6 +1,38 @@
 // "DOMContentLoaded" event'en sørger for, at Javascriptet først kører når hele HTML dokumentet er indlæst
 document.addEventListener("DOMContentLoaded", () => {
 
-	// skriv din kode her...
+
+
+  //metode nr.1:
+// Array:
+//let names = ['Anders', 'Fedtmule', 'Mickey'];
+//let ulElement = document.querySelector('#liste');
+
+//Loop:
+//names.forEach((navn) => {
+//  ulElement.innerHTML += `<li> ${navn} </li>`;
+//});
+
+
+//Metode nr. 2:
+// Array:
+let names = ['Anders', 'Fedtmule', 'Mickey'];
+
+//Loop:
+names.forEach((navn) => {
+  let ulElement = document.createElement('li');
+  document.querySelector('#liste').appendChild(ulElement);
+  ulElement.textContent = navn;
+  //ulElement.style.listStyle = 'none';
+});
+
+
+
+
+
+
+
+//document.getElementById('liste').innerHTML = myFunction;
+
 
 }); // Afslutter: DOMContentLoaded
